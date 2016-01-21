ba8d75a7ed9cbfa2816e29c6bdcc4c0595107715
Florian Schlegel <flo@Florians-MBP.fritz.box>
2016-01-21 15:51:11 +0100

    Implemented basic client server example for VatRequest

--- server.go
+++ server.go
@@ -1,39 +1,69 @@
 package soap
 
 import (
+	"encoding/xml"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
+
+// ########### REMOVE THIS ###############
+type CheckVatRequest struct {
+	XMLName xml.Name `xml:"urn:ec.europa.eu:taxud:vies:services:checkVat:types checkVat"`
+
+	CountryCode string `xml:"countryCode,omitempty"`
+	VatNumber   string `xml:"vatNumber,omitempty"`
+}
+
+type CheckVatResponse struct {
+	XMLName xml.Name `xml:"urn:ec.europa.eu:taxud:vies:services:checkVat:types checkVatResponse"`
+
+	CountryCode string `xml:"countryCode,omitempty"`
+	VatNumber   string `xml:"vatNumber,omitempty"`
+	RequestDate string `xml:"requestDate,omitempty"`
+	Valid       bool   `xml:"valid,omitempty"`
+	Name        string `xml:"name,omitempty"`
+	Address     string `xml:"address,omitempty"`
+}
+
+// ########### REMOVE THIS END ###############
 
 type OperationHandlerFunc func(request interface{}) (response interface{}, err error)
 type RequestFactoryFunc func() interface{}
 
-type operationHander struct {
+type operationHandler struct {
 	requestFactory RequestFactoryFunc
 	handler        OperationHandlerFunc
 }
 
 type Server struct {
-	handlers map[string]map[string]*operationHander
+	handlers map[string]map[string]*operationHandler
 }
 
 func NewServer() *Server {
 	s := &Server{
-		handlers: make(map[string]map[string]*operationHander),
+		handlers: make(map[string]map[string]*operationHandler),
 	}
 	return s
 }
 
 // HandleOperation register to handle an operation
 func (s *Server) HandleOperation(action string, messageType string, requestFactory RequestFactoryFunc, operationHandlerFunc OperationHandlerFunc) {
-	s.handlers[action][messageType] = &operationHander{
+	_, ok := s.handlers[action]
+	if !ok {
+		s.handlers[action] = make(map[string]*operationHandler) // is this good??
+	}
+
+	s.handlers[action][messageType] = &operationHandler{
 		handler:        operationHandlerFunc,
 		requestFactory: requestFactory,
 	}
 }
 
 func (s *Server) serveSOAP(requestEnvelopeBytes []byte, soapAction string) (responseEnvelopeBytes []byte, err error) {
-	messageType := "find me as the element name in the soap body"
+	messageType := "checkVatRequest"
+	soapAction = "operationCheckVat"
 	actionHandlers, ok := s.handlers[soapAction]
 	if !ok {
 		err = errors.New("could not find handlers for action: \"" + soapAction + "\"")
@@ -66,6 +96,54 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
 		w.Write([]byte("that actually could be a soap request"))
+		requestBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// // Retrieve the appropriate handler
+		// @todo be more careful retrieving the operation handler
+		operationHandlers, ok := s.handlers[""]
+		if !ok {
+			// soap fault
+			return
+		}
+		operationHandler, ok := operationHandlers["checkVatRequest"]
+		if !ok {
+			// soap fault
+			return
+		}
+		request := operationHandler.requestFactory()
+
+		envelope := &SOAPEnvelope{
+			Body: SOAPBody{
+				Content: request,
+			},
+		}
+		errUnm := xml.Unmarshal(requestBytes, envelope)
+
+		fmt.Println("Request Body", envelope, "Error:", errUnm)
+
+		response, errResp := operationHandler.handler(envelope.Body.Content)
+
+		if errResp != nil {
+			// soap fault
+			return
+		}
+
+		responseEnvelope := &SOAPEnvelope{
+			Body: SOAPBody{
+				Content: response,
+			},
+		}
+
+		xmlBytes, err := xml.MarshalIndent(responseEnvelope, "", "	")
+
+		if err != nil {
+			// soap fault
+			return
+		}
+		w.Write(xmlBytes)
 	default:
 		// this will be a soap fault !?
 		w.Write([]byte("this is a soap service - you have to POST soap requests\n"))
--- test/test.go
+++ test/test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"log"
 
 	"github.com/foomo/soap"
 )
@@ -14,25 +16,65 @@ type FooResponse struct {
 	Bar string
 }
 
+type CheckVatRequest struct {
+	XMLName xml.Name `xml:"urn:ec.europa.eu:taxud:vies:services:checkVat:types checkVat"`
+
+	CountryCode string `xml:"countryCode,omitempty"`
+	VatNumber   string `xml:"vatNumber,omitempty"`
+}
+
+type CheckVatResponse struct {
+	XMLName xml.Name `xml:"urn:ec.europa.eu:taxud:vies:services:checkVat:types checkVatResponse"`
+
+	CountryCode string `xml:"countryCode,omitempty"`
+	VatNumber   string `xml:"vatNumber,omitempty"`
+	RequestDate string `xml:"requestDate,omitempty"`
+	Valid       bool   `xml:"valid"`
+	Name        string `xml:"name,omitempty"`
+	Address     string `xml:"address,omitempty"`
+}
+
 func RunServer() {
 	soapServer := soap.NewServer()
-	/*
-		soapServer.HandleOperation(
-			"operationFoo",
-			"FooRequest",
-			func() interface{} {
-				return &FooRequest{}
-			},
-			func(request interface{}) (response interface{}, err error) {
-				fooRequest := request.(*FooRequest)
-				fooResponse := &FooResponse{
-					Bar: "Hello " + fooRequest.Foo,
-				}
-				response = fooResponse
-				return
-			},
-		)
-	*/
+
+	// soapServer.HandleOperation(
+	// 	"operationFoo",
+	// 	"FooRequest",
+	// 	func() interface{} {
+	// 		return &FooRequest{}
+	// 	},
+	// 	func(request interface{}) (response interface{}, err error) {
+	// 		fooRequest := request.(*FooRequest)
+	// 		fooResponse := &FooResponse{
+	// 			Bar: "Hello " + fooRequest.Foo,
+	// 		}
+	// 		response = fooResponse
+	// 		return
+	// 	},
+	// )
+
+	// Create handler for checkVat
+	soapServer.HandleOperation(
+		"",
+		"checkVatRequest",
+		func() interface{} {
+			r := &CheckVatRequest{}
+			log.Println("creating new vat request", r)
+			return r
+		},
+		func(request interface{}) (response interface{}, err error) {
+			log.Println("handling request", request)
+			checkVatRequest := request.(*CheckVatRequest)
+			checkVatResponse := &CheckVatResponse{
+				CountryCode: checkVatRequest.CountryCode,
+				VatNumber:   "lalala",
+				Valid:       false,
+			}
+			response = checkVatResponse
+			return
+		},
+	)
+
 	err := soapServer.ListenAndServe(":8080")
 	fmt.Println(err)
 }
